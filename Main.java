499d9655dd7b8f8399c97ad0fe76979feb9d68fb
Calum Johnston <CSJJohnston97@gmail.com>
2022-03-20 19:31:12 +0000

    Got the basis of the fetch for each item plus iterating need to add smooting and finalisation

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -10,28 +11,32 @@ import java.util.Scanner;
 public class Main {
 
     public static void main (String[] args) throws IOException, InterruptedException {
-        String url;                                             //initialise strings and ints to be used and the scanner to read StdIn
         int statusCode, contentLength;
+        String url;
         Scanner scanner = new Scanner(System.in);
 
-        url = scanner.next();                                   //Scanning for StdIn input of next URL
+        while(scanner.hasNextLine()){
+            url = scanner.nextLine();
+            if (url != "") {
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(url))
+                        .GET()
+                        .build();
+                HttpResponse<String> response =
+                        client.send(request, HttpResponse.BodyHandlers.ofString());
+                statusCode = response.statusCode();
+                contentLength = response.body().length();
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET()
-                .build();
-        HttpResponse<String> response =
-                client.send(request, HttpResponse.BodyHandlers.ofString());
-        statusCode = response.statusCode();
+                System.out.println("\"Url\": \"" + url + "\",");
+                System.out.println("\"Status-code\": \"" + statusCode + "\",");
+                System.out.println("\"Content-length\": \"" + contentLength + "\",");
+                Thread.sleep(500);
+            }
+        }
 
 
 
-
-
-        System.out.println("\"Url\": \""+ url + "\"," );
-        System.out.println("\"Status-code\": \""+ statusCode + "\"," );
-        System.out.println("\"Content-length\": \""+ contentLength + "\"," );
 
     }
 }
