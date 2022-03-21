8e106968faa1929aff48c04a3d0b552745492bd2
Calum Johnston <CSJJohnston97@gmail.com>
2022-03-21 10:28:34 +0000

    near final product, finished Json format and added ways to catch errors in the url or any dropped connection

--- Main.java
+++ Main.java
@@ -49,6 +49,12 @@ public class Main {
                     System.out.println("  \"Content-length\": \"" + contentLength + "\",");
                     System.out.println("}");
                     Thread.sleep(500);
+                } catch (IOException e) {
+                    url = "";
+                    System.out.println("Please enter a correct URL");                           //Catch errors that occur in the fetching of website data
+                } catch (InterruptedException e) {
+                    url = "";
+                    System.out.println("Please enter a correct URL");
                 } catch (IllegalArgumentException e) {
                     url = "";
                     System.out.println("Please enter a correct URL");
